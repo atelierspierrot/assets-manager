1dff6efc6191c383f63b194044869e5047d60a2e
Piero Wbmstr <me@e-piwi.fr>
2013-09-22 20:09:15 +0200

    Corrections in gitignore and Sami configuration

---
+++ sami.config.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Application documentation builder
+ *
+ * See http://github.com/fabpot/Sami
+ *
+ * To build doc, run:
+ *     $ php vendor/sami/sami/sami.php render sami.config.php
+ *
+ * To update it, run:
+ *     $ php vendor/sami/sami/sami.php update sami.config.php
+ *
+ */
+
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->notName('SplClassLoader.php')
+    ->in(__DIR__.'/src')
+;
+
+$options = array(
+    'title'                => 'Assets Manager',
+    'build_dir'            => __DIR__.'/phpdoc',
+    'cache_dir'            => __DIR__.'/../tmp/cache/assets-manager',
+    'default_opened_level' => 2,
+);
+
+return new Sami($iterator, $options);
+
+// Endfile
