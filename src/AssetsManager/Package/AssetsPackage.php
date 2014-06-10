364f76428c13bbf82613a4d4e4f3252035b0f6f6
Piero Wbmstr <me@e-piwi.fr>
2014-06-10 20:27:02 +0200

    new piwi github url + 2014 copyleft :(

--- src/AssetsManager/Package/AssetsPackage.php
+++ src/AssetsManager/Package/AssetsPackage.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * AssetsManager - Composer plugin
- * Copyleft (c) 2013 Pierre Cassat and contributors
+ * Copyleft (c) 2013-2014 Pierre Cassat and contributors
  * <www.ateliers-pierrot.fr> - <contact@ateliers-pierrot.fr>
  * License GPL-3.0 <http://www.opensource.org/licenses/gpl-3.0.html>
  * Sources <https://github.com/atelierspierrot/assets-manager>
@@ -19,7 +19,7 @@ use AssetsManager\Loader as AssetsLoader,
     AssetsManager\Package\Preset;
 
 /**
- * @author 		Piero Wbmstr <piero.wbmstr@gmail.com>
+ * @author 		Piero Wbmstr <me@e-piwi.fr>
  */
 class AssetsPackage extends AbstractAssetsPackage implements AssetsPackageInterface
 {
